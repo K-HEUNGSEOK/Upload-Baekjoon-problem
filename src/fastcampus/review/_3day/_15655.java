06455a9ff832ea6de04264fbb5d8d61e96dd1232
kimheungseok <eovnfjfpa@naver.com>
2025-07-30 14:24:58 +0900

    15655 S3 N과 M(6)

---
+++ src/fastcampus/review/_3day/_15655.java
@@ -0,0 +1,38 @@
+package fastcampus.review._3day;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _15655 {
+    static int n,m;
+    static int[] arr;
+    static int[] param;
+    static boolean[] ch;
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        param = new int[m];
+        ch = new boolean[n];
+        for(int i = 0; i < n ; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        DFS(0, 0);
+    }
+    static void DFS(int v, int start){
+        if (v == m){
+            for(int i = 0 ; i < m; i++){
+                System.out.print(param[i] + " ");
+            }
+            System.out.println();
+        }else{
+            for(int i = start ; i < n ; i ++){
+                param[v] = arr[i];
+                DFS(v + 1, i + 1);
+            }
+        }
+    }
+}
